2a8e4cb6e435af72b1fc93ec6394dd42776bbf56
Mason <Mudland2721@gamil.com>
2020-08-02 22:59:52 -0600

    still working on current error

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -2,10 +2,11 @@ console.log(`HERE WE GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO`);
 
 const FILES_TO_CACHE = [
   //want icons and manifest cached and ready
+  "/",
   "/index.html",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
-  "manifest.webmanifest",
+  "/manifest.webmanifest",
 ];
 
 const CACHE_NAME = "static-cache-v2";
@@ -31,7 +32,7 @@ self.addEventListener("active", function (evt) {
       return Promise.all(
         keyList.map((key) => {
           // if key inst V1 and V2 delete it
-          if (key === CACHE_NAME && key === DATA_CACHE_NAME) {
+          if (key === CACHE_NAME && key !== DATA_CACHE_NAME) {
             console.log("Removed old cache data", key);
             return caches.delete(key);
           }
@@ -57,7 +58,14 @@ self.addEventListener("fetch", function (evt) {
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
-
+              //   if (response) {
+              //     return response;
+              //   } else if (
+              //     event.request.headers.get("accept").includes("text/html")
+              //   ) {
+              //     // return the cached home page for all requests for html pages
+              //     return caches.match("/");
+              //   }
               return response;
             })
             .catch((err) => {
@@ -70,12 +78,6 @@ self.addEventListener("fetch", function (evt) {
 
     return;
   }
-
-  if (response) {
-    return response;
-  } else if (event.request.headers.get("accept").includes("text/html")) {
-    // return the cached home page for all requests for html pages
-    return caches.match("/");
-  }
 });
+
 //fetch evt list ============= end
